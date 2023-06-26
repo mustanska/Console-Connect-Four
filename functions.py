9a6abb57a2a16c0ac77016f5442711fa5bfc7b6c
mustanska <122823838+mustanska@users.noreply.github.com>
2023-06-26 23:05:37 +0300

    Added some new functions in functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,13 @@
 # A function that checks for a unique player name and sign
-def check_unique_value(current_value, current_list):
+def is_valid_column(current_column, current_list):
+    for row in range(len(current_list)):
+        if 0 <= current_column < len(current_list[row]):
+            return True
+    return False
+
+
+# A function that checks for a unique player name and sign
+def is_unique_value(current_value, current_list):
     if current_value not in current_list:
         return True
     return False
@@ -10,6 +18,15 @@ def is_empty_spaces_in_the_column(field, column):
     for row in range(len(field)):
         if field[row][column] == 0:
             return True
+    return False
+
+
+# A function that checks for empty space in field
+def is_empty_spaces(field):
+    for row in range(len(field)):
+        for col in range(len(field[row])):
+            if is_empty_spaces_in_the_column(field, col):
+                return True
     return False
 
 
@@ -25,3 +42,5 @@ def player_move(field, sign, column):
             row -= 1
 
     return field
+
+# A function that checks for the winner
