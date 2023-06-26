9f3b84db25ad7201de0c4922553dbdb0e2d9912d
mustanska <122823838+mustanska@users.noreply.github.com>
2023-06-26 23:08:07 +0300

    Made changes for more clean code in connect_four.py

--- connect_four.py
+++ connect_four.py
@@ -1,37 +1,23 @@
-from functions import check_unique_value, is_empty_spaces_in_the_column, player_move
-
-ROWS = 6    # the field rows
-COLUMNS = 7     # the field columns
-
-field = [[0] * COLUMNS for row in range(ROWS)]
-
-players = int(input("How many people will you play?"))
-
-# Create the dictionary with information about name of player and its sign
-players_signs = {}
-number = 1
-
-while number <= players:
-    name = input(f"Player {number} please write your name: ")
-    sign = input("Choose sign: ")
-
-    if check_unique_value(name, players_signs.keys()) and check_unique_value(sign, players_signs.values()):
-        players_signs[name] = sign
-        number += 1
-    else:
-        print("The name or the sign is already taken. Please write them again.")
+from functions import is_valid_column, is_empty_spaces, is_empty_spaces_in_the_column, player_move
+from parameters import field, ROWS, COLUMNS, players_signs
 
 # Start the game
-# TODO: make while loop stopping when the field is full
 # TODO: add the logic about the winner
-while True:
+while is_empty_spaces(field):
     for name, sign in players_signs.items():
         chosen_column = int(input(f"{name}, please choose the column from 1 to {COLUMNS}")) - 1
+
+        if not is_valid_column(chosen_column, field):
+            print(f"{name}, you miss your turn. The column is not valid.")
+            continue
 
         if is_empty_spaces_in_the_column(field, chosen_column):
             field = player_move(field, sign, chosen_column)
         else:
-            print("You miss your turn. The column is full.")
+            print(f"{name}, you miss your turn. The column is full.")
 
         [print(*row) for row in field]
+
+        if not is_empty_spaces(field):
+            break
 
