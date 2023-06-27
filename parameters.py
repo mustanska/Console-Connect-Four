5bab19740452843e18ba5b8de0635f0491c003d5
mustanska <122823838+mustanska@users.noreply.github.com>
2023-06-28 00:03:29 +0300

    Added a color to every player sign

--- parameters.py
+++ parameters.py
@@ -3,14 +3,17 @@ from validation_functions import is_unique_value, is_valid_input
 
 ROWS = 6    # the field rows
 COLUMNS = 7     # the field columns
+COLORS = [Fore.RED, Fore.BLUE, Fore.CYAN, Fore.MAGENTA, Fore.YELLOW]
 
 empty_field = [[0] * COLUMNS for row in range(ROWS)]
 
 while True:
-    players = input(Fore.BLACK + "How many people will you play?: ")
+    players = input(Fore.BLACK + "How many people will you play? Choose from 2 to 5: ")
+
     if is_valid_input(players):
         players = int(players)
-        break
+        if 1 < players <= 5:
+            break
 
     print(Fore.RED + "The input is not valid. Try again...")
 
@@ -24,7 +27,7 @@ while number <= players:
     sign = input("Choose sign: ")
 
     if is_unique_value(name, players_signs.keys()) and is_unique_value(sign, players_signs.values()):
-        players_signs[name] = sign
+        players_signs[name] = COLORS[number - 1] + sign + Fore.BLACK
         number += 1
     else:
         print(Fore.RED + "The name or the sign is already taken. Please write them again.")
