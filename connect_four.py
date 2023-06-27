c9933fb97fb70d04ade88c89008c0e53a0a78cb1
mustanska <122823838+mustanska@users.noreply.github.com>
2023-06-27 17:51:51 +0300

    Added a logic to restart the game

--- connect_four.py
+++ connect_four.py
@@ -1,6 +1,13 @@
+from copy import deepcopy
 from logic_functions import start_game
-from parameters import field
+from parameters import empty_field
 
-# TODO: add the logic about the winner
-start_game(field)
+start = input('Press "Y" if you want to start the game or something else if you want to leave:').lower()
 
+while start == "y":
+    field = deepcopy(empty_field)
+    start_game(field)
+
+    start = input('Press "Y" if you want to restart the game or "N" if you want to leave:').lower()
+else:
+    print("Bye. See you soon.")
