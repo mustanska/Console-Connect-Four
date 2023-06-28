955910e1fda39c86ca57577b9f6f41af511f5ee9
mustanska <122823838+mustanska@users.noreply.github.com>
2023-06-28 12:18:34 +0300

    Combined several identical functionalities in one function

--- logic_functions.py
+++ logic_functions.py
@@ -1,4 +1,4 @@
-from colorama import Fore
+from colorama import Fore, Back
 from parameters import players_signs, ROWS, COLUMNS
 from validation_functions import is_empty_spaces, is_valid_input, is_valid_column, is_empty_spaces_in_the_column
 
@@ -22,7 +22,7 @@ def winner_checking(field, current_row, current_col, current_sign):
 
     result.append(check_row(field, current_row, current_sign))
     result.append(check_column(field, current_col, current_sign))
-    result.append((check_primary_diagonal(field, current_row, current_col, current_sign)))
+    result.append(check_primary_diagonal(field, current_row, current_col, current_sign))
     result.append(check_secondary_diagonal(field, current_row, current_col, current_sign))
 
     if any(result):
@@ -31,110 +31,71 @@ def winner_checking(field, current_row, current_col, current_sign):
     return False
 
 
-# A function that checks if winner is at the given row
-def check_row(field, row, sign):
+# A function that checks positions (row, column, first and second diagonal
+def check_positions(field, row, col, sign, is_r=False, is_c=False, is_pd=False, is_sd=False):
     count = 0
     indices = []
 
-    for column in range(COLUMNS):
-        if field[row][column] == sign:
+    while True:
+        if field[row][col] == sign:
             count += 1
-            indices.append([row, column])
-
-        if 0 <= column + 1 < COLUMNS and count < 4:
-            if field[row][column + 1] != sign:
-                count = 0
-                indices = []
+            indices.append([row, col])
 
         if count == 4:
             for r, c in indices:
-                field[r][c] = Fore.GREEN + sign + Fore.BLACK
+                field[r][c] = Back.WHITE + sign + Back.RESET
             return True
 
-    return False
+        if is_r and 0 <= col + 1 < COLUMNS:
+            col += 1
+
+        elif is_c and 0 <= row + 1 < ROWS:
+            row += 1
+
+        elif is_pd and (0 <= row + 1 < ROWS and 0 <= col + 1 < COLUMNS):
+            row += 1
+            col += 1
+
+        elif is_sd and (0 <= row + 1 < ROWS and 0 <= col - 1 < COLUMNS):
+            row += 1
+            col -= 1
+
+        else:
+            return False
+
+        if field[row][col] != sign:
+            count = 0
+            indices = []
+
+
+# A function that checks if winner is at the given row
+def check_row(field, row, sign):
+    col = 0
+    return check_positions(field, row, col, sign, is_r=True)
 
 
 # A function that checks if winner is at the given column
-def check_column(field, column, sign):
-    count = 0
-    indices = []
-
-    for row in range(ROWS):
-        if field[row][column] == sign:
-            indices.append([row, column])
-            count += 1
-
-        if 0 <= row + 1 < ROWS and count < 4:
-            if field[row + 1][column] != sign:
-                count = 0
-                indices = []
-
-        if count == 4:
-            for r, c in indices:
-                field[r][c] = Fore.GREEN + sign + Fore.BLACK
-            return True
-
-    return False
+def check_column(field, col, sign):
+    row = 0
+    return check_positions(field, row, col, sign, is_c=True)
 
 
 # A function that checks if winner is at the primary diagonal
-def check_primary_diagonal(field, row, column, sign):
-    count = 0
-    indices = []
+def check_primary_diagonal(field, row, col, sign):
+    step = min(row, col)
+    row -= step
+    col -= step
 
-    step = min(row, column)
-    start_row = row - step
-    start_col = column - step
-
-    while 0 <= start_row < ROWS and 0 <= start_col < COLUMNS:
-        if field[start_row][start_col] == sign:
-            indices.append([start_row, start_col])
-            count += 1
-
-        if 0 <= start_row + 1 < ROWS and 0 <= start_col + 1 < COLUMNS and count < 4:
-            if field[start_row + 1][start_col + 1] != sign:
-                count = 0
-                indices = []
-
-        if count == 4:
-            for r, c in indices:
-                field[r][c] = Fore.GREEN + sign + Fore.BLACK
-            return True
-
-        start_row += 1
-        start_col += 1
-
-    return False
+    return check_positions(field, row, col, sign, is_pd=True)
 
 
 # A function that checks if winner is at the secondary diagonal
-def check_secondary_diagonal(field, row, column, sign):
-    count = 0
-    indices = []
+def check_secondary_diagonal(field, row, col, sign):
+    step = min(row, COLUMNS - 1 - col)
+    row -= step
+    col += step
 
-    step = min(row, COLUMNS - 1 - column)
-    start_row = row - step
-    start_col = column + step
-
-    while 0 <= start_row < ROWS and 0 <= start_col < COLUMNS:
-        if field[start_row][start_col] == sign:
-            indices.append([start_row, start_col])
-            count += 1
-
-        if 0 <= start_row + 1 < ROWS and 0 <= start_col - 1 < COLUMNS and count < 4:
-            if field[start_row + 1][start_col - 1] != sign:
-                count = 0
-                indices = []
-
-        if count == 4:
-            for r, c in indices:
-                field[r][c] = Fore.GREEN + sign + Fore.BLACK
-            return True
-
-        start_row += 1
-        start_col -= 1
-
-    return False
+    return check_positions(field, row, col, sign, is_sd=True)
 
 
 # A function for start the game
@@ -172,5 +133,5 @@ def start_game(field):
                 break
 
             if not is_empty_spaces(field):
-                print(Fore.BLUE + f"There is no more empty spaces. The game is over!")
+                print(Fore.BLUE + "There is no more empty spaces. The game is over!")
                 break
